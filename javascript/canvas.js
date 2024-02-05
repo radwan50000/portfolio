a6653bd44676fdb8b9da2b81e5d333f9e5576e45
Muhammed Elsayed <muhammedelsayedradwan5@gmail.com>
2024-02-05 23:40:09 +0200

    Adding changes in canvas game

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,123 +1,167 @@
-const c = document.getElementById("space-effect");
+const c = document.getElementById("space-effect"),
+  firstImage = "../images/monster.png",
+  secondImage = "../images/monster2.png",
+  thirdImage = "../images/monster3.png";
+
+let bulletNo = 0;
 
 c.width = window.innerWidth;
 if (window.innerWidth > 600) {
-    c.height = window.innerHeight / 1.3;
+  c.height = window.innerHeight / 1.3;
 } else {
-    c.height = window.innerHeight * 0.5;
+  c.height = window.innerHeight * 0.5;
 }
 
-const ctx = c.getContext('2d');
-
+const ctx = c.getContext("2d");
 
 function getRandomNumber(max) {
   return Math.floor(Math.random() * (max - 20 + 1)) + 20;
 }
 
-let firstImage = '../images/monster.png';
-let secondImage = '../images/monster2.png';
-let thirdImage = '../images/monster3.png';
+class monsterImage {
+  constructor(x, src, sizeW, sizeH) {
+    this.x = x;
+    this.y = getRandomNumber(600);
+    this.dy = 2;
+    this.src = src;
+    this.sizeW = sizeW;
+    this.sizeH = sizeH;
+  }
 
-class monsterImage{
+  draw = function () {
+    let image = new Image();
+    image.src = this.src;
+    ctx.drawImage(image, this.x, this.y, this.sizeW, this.sizeH);
+  };
 
-    constructor(x,src,sizeW,sizeH){
-        this.x = x;
-        this.y = getRandomNumber(600);
-        this.dy = 2;
-        this.src = src;
-        this.sizeW = sizeW;
-        this.sizeH = sizeH;
+  update = function () {
+    this.draw();
+    this.y += this.dy;
+    if (this.y > c.height) {
+      this.x = getRandomNumber(c.width - 100);
+      this.y = -80;
     }
-    
-    draw = function () {
-        let image = new Image();
-        image.src = this.src;
-        ctx.drawImage(image, this.x, this.y, this.sizeW, this.sizeH);
-    }
-
-    update = function () {
-        this.draw();
-        this.y += this.dy;
-        if (this.y > c.height) {
-            this.x = getRandomNumber(c.width - 100);
-            this.y = -80;
-        }
-    }
+  };
 }
 
-class Rocket{
+class Rocket {
+  constructor() {
+    this.x = c.width / 2 - 40;
+    this.dx = 5;
+  }
 
-    constructor() {
-        this.x = c.width / 2 - 40;
-        this.dx = 5;
-    }
+  draw() {
+    let rocket = new Image();
+    rocket.src = "../images/rocket.png";
+    ctx.drawImage(rocket, this.x, c.height - 100, 40, 40);
+  }
 
-    draw() {
-        let rocket = new Image();
-        rocket.src = '../images/rocket.png';
-        ctx.drawImage(rocket, this.x, c.height - 100, 40, 40);
-    }
-
-    update(e) {
-        this.x = e.pageX;
-        this.draw();
-    }
+  update(e) {
+    this.x = e.pageX;
+    this.draw();
+  }
 }
+
+class Bullet {
+  constructor() {
+    this.x;
+    this.y = c.height - 100;
+    this.dy = 0;
+    this.bulletNo = 0;
+  }
+
+  draw = function () {
+    this.y += this.dy;
+    let rocket = new Image();
+    rocket.src = "../images/fire.png";
+    ctx.drawImage(rocket, this.x + 15, this.y, 15, 15);
+  };
+
+  updateBullet = function (e) {
+    this.x = e.pageX;
+    if (this.y > -10) {
+      this.dy = -15;
+    } else {
+      this.dy = 0;
+    }
+  };
+}
+
+changeBullet = function () {
+  bulletNo++;
+  if (bullets.length > 6) {
+    bullets = [];
+    bulletNo = 0;
+  }
+  bullets.push(new Bullet());
+  return bulletNo;
+};
 
 let monsters = [];
 
+let bullets = [new Bullet()];
+
 let rocket = new Rocket();
 
-c.addEventListener('mousemove', function (e) {
-    rocket.update(e);
+c.addEventListener("mousemove", function (e) {
+  rocket.update(e);
 });
 
-c.addEventListener('touchmove', function (e) {
-    rocket.update(e);
+c.addEventListener("touchmove", function (e) {
+  rocket.update(e);
+});
+
+c.addEventListener("click", function (e) {
+  bullets[changeBullet()].updateBullet(e);
 });
 
 if (c.width > 600) {
-    let num = 4;
-    let size = 20;
-    for (let i = 0; i < num; i++){
-        monsters.push(new monsterImage(getRandomNumber(c.width),firstImage,size,size));
-    }
+  let num = 4;
+  let size = 20;
+  for (let i = 0; i < num; i++) {
+    monsters.push(
+      new monsterImage(getRandomNumber(c.width), firstImage, size, size)
+    );
+  }
 
-    for (let i = 0; i < num; i++){
-        monsters.push(new monsterImage(getRandomNumber(c.width),secondImage,size,size));
-    }
+  for (let i = 0; i < num; i++) {
+    monsters.push(
+      new monsterImage(getRandomNumber(c.width), secondImage, size, size)
+    );
+  }
 
-    for (let i = 0; i < num; i++){
-        monsters.push(new monsterImage(getRandomNumber(c.width),thirdImage,size,size));
-    }
+  for (let i = 0; i < num; i++) {
+    monsters.push(
+      new monsterImage(getRandomNumber(c.width), thirdImage, size, size)
+    );
+  }
 } else {
-    let num = 2;
-    let size = 15;
-    for (let i = 0; i < num; i++){
-        monsters.push(new monsterImage(getRandomNumber(c.width),firstImage,size,size));
-    }
-
-    for (let i = 0; i < num; i++){
-        monsters.push(new monsterImage(getRandomNumber(c.width),secondImage,size,size));
-    }
-
-    for (let i = 0; i < num; i++){
-        monsters.push(new monsterImage(getRandomNumber(c.width),thirdImage,size,size));
-    }
+  let num = 2;
+  let size = 15;
+  for (let i = 0; i < num; i++) {
+    monsters.push(
+      new monsterImage(getRandomNumber(c.width), firstImage, size, size)
+    );
+    monsters.push(
+      new monsterImage(getRandomNumber(c.width), secondImage, size, size)
+    );
+    monsters.push(
+      new monsterImage(getRandomNumber(c.width), thirdImage, size, size)
+    );
+  }
 }
 
+function canvasAnimate() {
+  requestAnimationFrame(canvasAnimate);
+  ctx.clearRect(0, 0, c.width, c.height);
 
-function canvasAnimate(e) {
-    requestAnimationFrame(canvasAnimate);
-    ctx.clearRect(0, 0, c.width, c.height);
+  for (let i = 0; i < monsters.length; i++) {
+    monsters[i].update();
+  }
 
-    for (let i = 0; i < monsters.length; i++) {
-        monsters[i].update();
-    }
+  bullets[bulletNo].draw();
 
-    rocket.draw();
-
+  rocket.draw();
 }
 
 canvasAnimate();
-
